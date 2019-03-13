777a070bbf56d5a47fe41cad4c6877c7c4175ba3
sylviopedroza <sylvio.pedroza@skipthedishes.ca>
2019-03-13 14:39:14 -0600

    fix for problems

--- guessit.py
+++ guessit.py
@@ -3,11 +3,11 @@ print("Welcome to the guessing game!")
 print("*****************************")
 
 secret_number = 35
-no_tentatives = 1
+no_tentatives = 3
 
 #while no_tentatives <= 3:
-for rodada in range(1, 3):
-    print("Tentatives {} of {} ".format(no_tentatives, "3"))
+for rodada in range(1, no_tentatives+1):
+    print("Tentatives {} of {} ".format(rodada, no_tentatives))
 
     user_input = int(input("Type a number: ", ))
     print("You typed ", user_input)
@@ -20,7 +20,6 @@ for rodada in range(1, 3):
         print("You won! :)")
         break
     else:
-        no_tentatives = no_tentatives+1
         if(higher):
             print("You guess was higher")
         elif(lesser):
