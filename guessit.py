9a1bb44d4961fa8f937ea592a3f39774be2661b5
sylviopedroza <sylvio.pedroza@skipthedishes.ca>
2019-03-12 20:27:34 -0600

    add break and tentative counting

--- guessit.py
+++ guessit.py
@@ -3,10 +3,10 @@ print("Welcome to the guessing game!")
 print("*****************************")
 
 secret_number = 35
-no_tentatives = 3
+no_tentatives = 1
 
-while no_tentatives > 0:
-    print("Remaining tentatives ", no_tentatives)
+while no_tentatives <= 3:
+    print("Tentatives {} of {} ".format(no_tentatives, "3"))
     user_input = int(input("Type a number: ", ))
     print("You typed ", user_input)
 
@@ -16,9 +16,9 @@ while no_tentatives > 0:
 
     if(user_won):
         print("You won! :)")
-        no_tentatives = 0
+        break
     else:
-        no_tentatives = no_tentatives-1
+        no_tentatives = no_tentatives+1
         if(higher):
             print("You guess was higher")
         elif(lesser):
