56cc9f85d1216f7eb6d284aba6504eb6fd6781a9
sylviopedroza <sylvio.pedroza@skipthedishes.ca>
2019-03-13 15:31:03 -0600

    validate input and use random for secret number

--- guessit.py
+++ guessit.py
@@ -1,16 +1,26 @@
+# author: sylvio.pedroza@gmail.com
+import random
+
 print("*****************************")
 print("Welcome to the guessing game!")
 print("*****************************")
 
-secret_number = 35
+secret_number = random.randrange(1, 101);
 no_tentatives = 3
 
-#while no_tentatives <= 3:
 for rodada in range(1, no_tentatives+1):
     print("Tentatives {} of {} ".format(rodada, no_tentatives))
 
-    user_input = int(input("Type a number: ", ))
+    user_input = input("Type a number between 1 and 100: ")
+    if len(user_input) == 0:
+        continue
+        
+    user_input = int(user_input)
     print("You typed ", user_input)
+
+    if user_input < 1 or user_input > 100:
+        print("Number out of the range ", user_input)
+        continue # go to next iteration
 
     user_won = user_input == secret_number
     higher = user_input > secret_number
@@ -18,11 +28,13 @@ for rodada in range(1, no_tentatives+1):
 
     if(user_won):
         print("You won! :)")
-        break
+        break #step out the loop
     else:
         if(higher):
             print("You guess was higher")
         elif(lesser):
             print("You guess was lesser")
+
+print("The secret number was {}".format(secret_number))
 
 print("End of game")
